d5620be6c58972cd0a9bcd4f6c834a8bfaec53b0
unknown <simonouyang@gmail.com>
2015-06-06 21:48:16 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'); //load the module, express will load ejs
+var bodyParser = require('body-parser'); //use the middleware;
+var path = require('path'); //standard loading
+
+//configure the app, 
+var app = express(); // app is the function
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//use the middleware
+//expressjs.com API reference Request/Middle ware older is important
+app.use(express.static(path.join(__dirname, 'bower_components'))); //tell express to serach the file
+app.use(express.static(path.join(__dirname, 'css'))); //tell express to serach the file
+app.use(bodyParser()); //register the middleware
+
+//define routes in todo.js, default in node_modules
+app.use(require('./todo'));
+//start the server
+var port = process.env.PORT || 1337;
+app.listen(port, function() {console.log("ready on port: " + port);});
---
+++ readme.md
@@ -0,0 +1,22 @@
+step 1: download nodejs and install it
+
+step 2: Verify Node is installed.
+
+in the command line, type "node -v", it should return the version number of node
+
+step 3: Install express
+
+in the command line, type "npm i express"
+
+step 4: Start the server, 
+
+in the command line, type "node app"
+
+Step 5: View the page
+
+localhost:l337, you will see the similar screen specified in screenshot/step1.jpg
+
+Step 6: Input category and description
+Sample input is specified in screenshot/step2.jpg
+Step 7: Click "Add new item"
+you will see the similar screen specified in screenshot/step3.jpg
